b9c91bcb819dd1a93532e2ea98e38615d1213710
Mike Zhang <mzseattle@gmail.com>
2018-01-15 13:17:33 -0800

    done

--- Python-Algorithms/StacksQueuesDeques/01_BalancedParenthesesCheck.py
+++ Python-Algorithms/StacksQueuesDeques/01_BalancedParenthesesCheck.py
@@ -7,8 +7,27 @@
 # You can assume the input string has no spaces.
 
 #region my implementation
+# ============= Note ========
+# use set to setup the matches pair
+# use list [] as a stack
 def balance_check(s):
-    pass
+    def isMatch(c1, c2):
+        matches = set([('(',')'),('{','}'),('[',']')])
+        if (c1, c2) in matches:
+            return True
+        return False 
+            
+    if len(s) % 2 != 0: 
+        return False
+    
+    stack = []
+    # opens = set('{[(')
+    for c in s:
+        if len(stack) != 0 and isMatch(stack[-1], c):
+            stack.pop()
+        else: 
+            stack.append(c)
+    return len(stack) == 0
 #endregion
 
 def test_balance_check():
@@ -17,3 +36,5 @@ def test_balance_check():
         assert stub('[](){([[[]]])}(') == False
         assert stub('[{{{(())}}}]((()))') == True
         assert stub('[[[]])]') == False
+        assert stub('][()') == False
+        assert stub('[][()]') == True
